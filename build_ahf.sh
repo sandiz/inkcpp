d1bcaf4a4948b4689226d1a3bc17c8069a7b3011
Sandi Chakravarty <sandi.chakravarty@gmail.com>
2023-06-12 02:14:05 -0700

    update bash script

--- build_ahf.sh
+++ build_ahf.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+argument=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+
+# Set the build configuration based on the argument
+if [[ $argument == "debug" ]] 
+then
+    configuration="Debug"
+elif [[ $argument == "release" ]]
+then
+    configuration="Release"
+else
+    configuration="Release"
+fi
+
+path='/Users/sandi/Projects/AHF/Source/ThirdParty/libink/osx64/'
+
+
+echo "Building inkcpp $configuration version..."
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install
-cmake --build . --config Release --target clean
-cmake --build . --config Release
-cmake --install . --component lib --prefix='/Users/sandi/Projects/AHF/Source/ThirdParty/libink/osx64/'
+cmake .. -DCMAKE_BUILD_TYPE=$configuration -DCMAKE_INSTALL_PREFIX=../install
+cmake --build . --config $configuration --target clean
+cmake --build . --config $configuration
+
+echo "Moving built files to $path."
+cmake --install . --component lib --prefix=$path
